0072e143191604e5529ce4d4273e8481e664d305
Vladimir Jelicic <vladimir.jelincic@gmail.com>
2018-01-23 22:24:16 +0100

    Initial commit

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class WelcomeScreen extends Component {
+  render() {
+    console.log('rendering welcome');
+    return (
+      <View>
+        <Text>WelcomeScreen</Text>
+        <Text>WelcomeScreen</Text>
+        <Text>WelcomeScreen</Text>
+        <Text>WelcomeScreen</Text>
+        <Text>WelcomeScreen</Text>
+        <Text>WelcomeScreen</Text>
+      </View>
+    );
+  }
+}
+export default WelcomeScreen;
